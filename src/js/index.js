a389cc6a55e6e8a16289008511d1c87c838deca8
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-03-14 22:06:42 -0400

    first pass
    plugging in Javascript and footer and header added.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
 // Javascript Entry Point
+import $ from 'jquery';
+
+import items from './items';
+
+import { input, select, area } from './template';
+
+var $app = $('.app');
+
+items.forEach(function(item) {
+	if (item.type === "textarea") {
+		template === area;
+	} else if (item.type === "select") {
+		template === select;
+	} else {
+		template === input;
+	}
+
+	var form = template(item);
+	$app.append ( form );
+});
+
+
+
+
--- src/js/template.js
+++ src/js/template.js
@@ -1,6 +1,23 @@
-export function(input) {
+export function info(input) {
 	return `
 		<div class="input">
 			<input type=${user.type} id=${user.id} />
 			<label for=${user.id}><span>${user.label}</span</label>
+			</div>
+			`}
+
+export function info(select) {
+	return `
+		<div class="select">
+			<label for=${user.id}></label>
+			<select id=${user.id}/>
+				<option value=${user.value}>${user.label}</option>
+			</div>
+			`}
+
+export function info(area) {
+	return `
+		<div class="text">
+			<textarea name=${user.type} rows="10 cols="50>${user.label}</textarea>
+			</div>
 			`}
